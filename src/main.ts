e90dd77d4a3ab51da77411cc7ffb45128c4a6fdd
SachinthaAbeygunasekara <112684802+SachinthaAbeygunasekara@users.noreply.github.com>
2024-10-29 21:34:59 +0530

    Separate component is created to view the notes in detail. And back to list button add in this template

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
 import 'zone.js';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { provideRouter, Routes } from '@angular/router';
+import { NotesListComponent } from './notes-list/notes-list.component';
+import { AddNoteComponent } from './add-note/add-note.component';
+import { NoteDetailComponent } from './note-detail/note-detail.component';
 
-bootstrapApplication(AppComponent);
+const routes: Routes = [
+  { path: '', component: NotesListComponent },
+  { path: 'new', component: AddNoteComponent },
+  { path: 'note/:id', component: NoteDetailComponent },
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)],
+});
